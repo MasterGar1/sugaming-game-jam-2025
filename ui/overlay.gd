661fd580771ef55212fe2f440ccb34e1f44cb5ee
Viktor Pazhev <asusvikp@gmail.com>
2025-05-18 00:50:00 +0300

    Add score label

--- ui/overlay.gd
+++ ui/overlay.gd
@@ -2,8 +2,10 @@ extends Control
 
 @onready var sacrifice_progress := $SacrificeProgress
 @onready var healthbar := $Healthbar
+@onready var score := $Score
 
 func _process(delta: float) -> void:
 	sacrifice_progress.value = get_parent().sacrifice.timer.time_left / Global.BASE_LEVELUP_COOLDOWN * 100
 	var player: Player = get_tree().get_nodes_in_group("player")[0]
 	healthbar.value = player.health / player.max_health * 100
+	score.text = "Score %s" % Global.score
