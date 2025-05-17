0a4822546c2543704b9dd2cc9c91d35a69617450
MasterGari <hristovgavril296@gmail.com>
2025-05-17 11:48:52 +0300

    Simple Pathfinding

--- entities/enemy.gd
+++ entities/enemy.gd
@@ -1,2 +1,18 @@
 extends Entity
 class_name Enemy
+
+@onready var navigation: NavigationAgent2D = $NavigationAgent2D
+@onready var player: Player = get_tree().get_nodes_in_group('player').front()
+
+func _physics_process(delta: float) -> void:
+	follow_player(delta)
+
+func follow_player(delta: float) -> void:
+	var dir: Vector2 = to_local(navigation.get_next_path_position())
+	velocity = dir.normalized() * speed * delta
+	move_and_slide()
+	make_path()
+
+func make_path() -> void:
+	navigation.target_position = player.global_position
+	
