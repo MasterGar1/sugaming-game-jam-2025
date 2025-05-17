53e11a7f5df04f87a53f17b90efbd28b8590bcef
Viktor Pazhev <asusvikp@gmail.com>
2025-05-17 19:00:11 +0300

    Add skill sacrifice

--- components/skill_system.gd
+++ components/skill_system.gd
@@ -1,10 +1,13 @@
 extends Node
 
 @onready var timer := $Timer
+@onready var popup := $PopupPanel
+@onready var buttons_grid := $PopupPanel/VBoxContainer/GridContainer
 @onready var player_skills: Node = player.get_node("Skills")
 
 @export var player: Player
 @export var skills: Array[PackedScene]
+@export var skill_button: PackedScene
 
 func _ready() -> void:
 	timer.timeout.connect(sacrifice_skill)
@@ -13,7 +16,22 @@ func _ready() -> void:
 		player_skills.add_child(skill.instantiate())
 	
 func sacrifice_skill() -> void:
-	player_skills.remove_child(player_skills.get_children().pick_random())
+	popup.popup_centered()
+	var current_skills = player_skills.get_children()
+	
+	for i in range(current_skills.size()):
+		var button = skill_button.instantiate()
+		button.text = current_skills[i].skill_name
+		buttons_grid.add_child(button)
+		button.pressed.connect(end_sacrifice.bind(i))
+
+func end_sacrifice(id: int) -> void:
+	player_skills.remove_child(player_skills.get_children()[id])
 	
 	for skill in player_skills.get_children():
 		skill.level_up()
+		
+	for child in buttons_grid.get_children():
+		child.queue_free()
+		
+	popup.hide()
--- skills/skill_base.gd
+++ skills/skill_base.gd
@@ -2,6 +2,7 @@ extends Node
 class_name Skill
 
 @export var level: int = 1
+@export var skill_name: String
 
 func level_up():
 	level += 1
