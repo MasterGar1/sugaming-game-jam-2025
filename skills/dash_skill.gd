c2dcb7b73463755675b4ea94885cda0e326e22b2
Viktor Pazhev <asusvikp@gmail.com>
2025-05-17 16:39:46 +0300

    Dash level up

--- skills/dash_skill.gd
+++ skills/dash_skill.gd
@@ -1,20 +1,21 @@
 extends Skill
 
+const DASH_SPEED: float = 20
+
 @onready var cooldown := $Cooldown
 @onready var collider := $Hitbox/CollisionShape2D
 @onready var actor = get_parent().get_parent()
+@onready var count: int = level
 
 @export var base_duration: float = 20
-@export var level: int = 1
 
-var count: int = 1
 var duration: float = base_duration
 var damage: int = 5
 
 func try_dash() -> void:
-	var direction = Input.get_vector("left", "right", "up", "down") * 20
+	var direction = Input.get_vector("left", "right", "up", "down") * DASH_SPEED
 	
-	if count <= 0 or direction.length() == 0:
+	if count <= 0 or direction.length() == 0 or not collider.disabled:
 		return
 		
 	count -= 1
@@ -35,6 +36,7 @@ func end_dash():
 	collider.disabled = true
 
 func _ready():
+	print(count)
 	cooldown.timeout.connect(reload)
 	actor.end_movement_lock.connect(end_dash)
 	collider.disabled = true
--- skills/skill_base.gd
+++ skills/skill_base.gd
@@ -1,2 +1,7 @@
 extends Node
 class_name Skill
+
+@export var level: int = 1
+
+func level_up():
+	level += 1
